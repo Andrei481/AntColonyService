295a5943381bb1f396f568f29f2034daa7e12d26
andrei-lazarov <andrei.lazarov@student.upt.ro>
2023-11-27 00:38:35 +0200

    Basic consumer
    Prints the number of received messages every 5 seconds

---
+++ src/main/java/SimulationEventType.java
@@ -0,0 +1,10 @@
+/* Keep up to date with the main app */
+public enum SimulationEventType {
+    BIRTH,
+    MEAL,
+    FOOD_CREATED,
+    FOOD_REDUCED,
+    FOOD_DEPLETED,
+    REPRODUCTION,
+    DEATH
+}
